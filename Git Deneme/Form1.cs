2f4ad6e4621fb14f399a3b37ed0288c679f1c989
Devrim Mert Yöyen <dmakcakavakli@gmail.com>
2020-07-24 16:30:38 +0300

    deneme commit devrim

--- Git Deneme/Form1.cs
+++ Git Deneme/Form1.cs
@@ -21,5 +21,10 @@ namespace Git_Deneme
         {
             MessageBox.Show("Base Button");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DMY");
+        }
     }
 }

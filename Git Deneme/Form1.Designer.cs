276d57ad84cb759ab080d7a06f3579e257f1c599
Çağatay Kalsan <59140367+cgtyklsn09@users.noreply.github.com>
2020-08-17 11:06:54 +0300

    buton eklendi

--- Git Deneme/Form1.Designer.cs
+++ Git Deneme/Form1.Designer.cs
@@ -30,14 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.devrimButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 31);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(32, 38);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 70);
+            this.button1.Size = new System.Drawing.Size(149, 86);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,23 +46,34 @@
             // 
             // devrimButton
             // 
-            this.devrimButton.Location = new System.Drawing.Point(193, 31);
-            this.devrimButton.Margin = new System.Windows.Forms.Padding(2);
+            this.devrimButton.Location = new System.Drawing.Point(257, 38);
+            this.devrimButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.devrimButton.Name = "devrimButton";
-            this.devrimButton.Size = new System.Drawing.Size(112, 70);
+            this.devrimButton.Size = new System.Drawing.Size(149, 86);
             this.devrimButton.TabIndex = 1;
             this.devrimButton.Text = "button2";
             this.devrimButton.UseVisualStyleBackColor = true;
             this.devrimButton.Click += new System.EventHandler(this.devrimButton_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(374, 91);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 288);
+            this.ClientSize = new System.Drawing.Size(545, 354);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.devrimButton);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -72,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button devrimButton;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Git Deneme/Form1.cs
+++ Git Deneme/Form1.cs
@@ -26,5 +26,10 @@ namespace Git_Deneme
         {
             MessageBox.Show("Devrim");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çato Button");
+        }
     }
 }
